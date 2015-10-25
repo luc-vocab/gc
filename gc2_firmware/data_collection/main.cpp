150c9ddb234df0082c5dd294b9a4ded9d967e560
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-10-25 14:30:34 +0800

    working batch data logging

--- gc2_firmware/data_collection/main.cpp
+++ gc2_firmware/data_collection/main.cpp
@@ -10,20 +10,33 @@ LSM9DS1 imu;
 #define LSM9DS1_AG	0x6B
 
 
+
 double battery_voltage = 0;
 double battery_soc = 0;
 bool serial_debug = true;
 bool realtime_send = false;
+bool batch_send = false;
 
 TCPClient gc_client;
 
-char send_buffer[500];
+#define HANDSHAKE_BUFFER_LENGTH 64
+#define BUFFER_LENGTH 5766
+char handshake_buffer[HANDSHAKE_BUFFER_LENGTH];
+char send_buffer[BUFFER_LENGTH];
+uint32_t current_offset = 0;
+uint16_t num_datapoints = 0;
+
+#define GC_MODE_REALTIME 1
+#define GC_MODE_BATCH 2
 
 // declare functions
 int gc_server_connect(String command);
 int gc_server_disconnect(String command);
 int gc_server_send_data(String command);
-int enable_realtime_send(String command);
+int set_mode(String command);
+
+
+void reset_batch_buffer();
 
 void setup() {
   pinMode(A0, INPUT);
@@ -47,7 +60,7 @@ void setup() {
   Particle.function("gc_conn", gc_server_connect);
   Particle.function("gc_disc", gc_server_disconnect);
   Particle.function("gc_send_1", gc_server_send_data);
-  Particle.function("realtime", enable_realtime_send);
+  Particle.function("set_mode", set_mode);
 
 
   // setup battery gauge
@@ -78,54 +91,47 @@ int gc_server_disconnect(String command) {
   return 0;
 }
 
-void write_int_to_buffer(char *buffer, int number, int *offset) {
-  /*
-  for (int i = 0; i < 4; i++)
-      buffer[3 - i + *offset] = (number >> (i * 8));
-  */
+void write_int_to_buffer(char *buffer, int number, uint32_t *offset) {
   memcpy(buffer + *offset, &number, sizeof(number));
   *offset += sizeof(number);
 }
 
-void write_float_to_buffer(char *buffer, float number, int *offset) {
+void write_float_to_buffer(char *buffer, float number, uint32_t *offset) {
   memcpy(buffer + *offset, &number, sizeof(number));
   *offset += sizeof(number);
 }
 
-void write_int16_to_buffer(char *buffer, uint16_t number, int *offset) {
+void write_int16_to_buffer(char *buffer, uint16_t number, uint32_t *offset) {
   memcpy(buffer + *offset, &number, sizeof(number));
   *offset += sizeof(number);
 }
 
-void write_long_to_buffer(char *buffer, unsigned long number, int *offset) {
+void write_long_to_buffer(char *buffer, unsigned long number, uint32_t *offset) {
   memcpy(buffer + *offset, &number, sizeof(number));
   *offset += sizeof(number);
 }
 
-int gc_server_send_data(String command) {
+void gc_server_initial_handshake(uint32_t mode) {
   if(serial_debug) {
-      Serial.println("gc_server_send_data");
+      Serial.println("gc_server_initial_handshake");
   }
 
-  int offset = 0;
+  uint32_t offset = 0;
 
   // write device id
   uint32_t device_id = 42;
-  write_int_to_buffer(send_buffer, device_id, &offset);
+  write_int_to_buffer(handshake_buffer, device_id, &offset);
   // write protocol version
   uint32_t protocol_version = 100;
-  write_int_to_buffer(send_buffer, protocol_version, &offset);
-  // write realtime mode
-  uint32_t mode = 1;
-  write_int_to_buffer(send_buffer, mode, &offset);
+  write_int_to_buffer(handshake_buffer, protocol_version, &offset);
+  // write mode (batch or realtime)
+  write_int_to_buffer(handshake_buffer, mode, &offset);
 
-  if(serial_debug) {
-      Serial.println("sending data: " + String(offset));
-  }
-
-
-  gc_client.write((const uint8_t *) send_buffer, offset);
+  gc_client.write((const uint8_t *) handshake_buffer, offset);
   gc_client.flush();
+}
+
+int gc_server_send_data(String command) {
 
   return 0;
 }
@@ -147,7 +153,7 @@ void get_accel(float *accel_values) {
   accel_values[2] = imu.calcAccel(imu.az);
 }
 
-void collect_datapoint_append_buffer(int *offset) {
+void collect_datapoint_append_buffer(uint32_t *offset) {
   // collect a data point of each type and write to the buffer
 
   // write timestamp in two parts, program startup time and millis
@@ -175,11 +181,25 @@ void collect_datapoint_append_buffer(int *offset) {
 
 }
 
-int enable_realtime_send(String command) {
-  if(command == "0") {
+int set_mode(String command) {
+  if(command == "batch") {
+    if(serial_debug) {
+        Serial.println("enabled batch mode");
+    }
     realtime_send = false;
-  } else {
+    batch_send = true;
+
+    reset_batch_buffer();
+
+  } else if(command == "realtime") {
     realtime_send = true;
+    batch_send = false;
+  } else {
+    if(serial_debug) {
+        Serial.println("disabling realtime and batch mode");
+    }
+    realtime_send = false;
+    batch_send = false;
   }
 
   return 0;
@@ -192,7 +212,7 @@ int gc_server_send_data_point() {
   }
 
   // write data to buffer
-  int offset = 0;
+  uint32_t offset = 0;
   collect_datapoint_append_buffer(&offset);
 
   if(serial_debug) {
@@ -205,31 +225,69 @@ int gc_server_send_data_point() {
   return 0;
 }
 
+void reset_batch_buffer() {
+  current_offset = 0;
+  num_datapoints = 0;
+}
+
+void gc_server_upload_batch() {
+  if(serial_debug) {
+      Serial.println("gc_server_upload_batch");
+  }
+
+  // connect to GC server
+  gc_client.connect("dev2.photozzap.com", 7001);
+  // indicate that we're uploading a batch
+  gc_server_initial_handshake(GC_MODE_BATCH);
+
+  // write data header
+  uint32_t temp_offset = 0;
+  // write battery charge
+  uint16_t percent_charged = lipo.getSOC() * 100.0;
+  write_int16_to_buffer(handshake_buffer, percent_charged, &temp_offset);
+  // indicate how many datapoints we're sending
+  write_int16_to_buffer(handshake_buffer, num_datapoints, &temp_offset);
+  // indicate total buffer size
+  write_int_to_buffer(handshake_buffer, current_offset, &temp_offset);
+  gc_client.write((const uint8_t *) handshake_buffer, temp_offset);
+  gc_client.flush();
+
+
+
+  // send full data buffer
+  gc_client.write((const uint8_t *) send_buffer, current_offset);
+  gc_client.flush();
+
+  reset_batch_buffer();
+
+  // disconnect from GC server
+  gc_client.stop();
+
+
+
+}
+
 
 void loop() {
+
+  if(batch_send) {
+    int previous_offset = current_offset;
+    collect_datapoint_append_buffer(&current_offset);
+    num_datapoints++;
+    int data_size = current_offset - previous_offset;
+    if(current_offset + data_size > BUFFER_LENGTH) {
+      // time to send buffer to GC server
+      if(serial_debug) {
+        Serial.println("filled up buffer, offset: " + String(current_offset) +
+                       " datapoints: " + String(num_datapoints));
+      }
+      gc_server_upload_batch();
+    }
+  }
 
   if(realtime_send) {
     gc_server_send_data_point();
   }
-
-  /*
-  battery_voltage = lipo.getVoltage();
-  battery_soc = lipo.getSOC();
-
-  if(serial_debug) {
-    Serial.print("Voltage: ");
-  	Serial.print(battery_voltage);  // Print the battery voltage
-  	Serial.println(" V");
-
-  	Serial.print("Percentage: ");
-  	Serial.print(battery_soc); // Print the battery state of charge
-  	Serial.println(" %");
-  	Serial.println();
-  }
-
-
-  delay(1000);
-  */
 
   delay(250);
 
