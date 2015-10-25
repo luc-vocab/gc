8f29b1b7bea1b49ba146c4310e0037cadff6adc0
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-10-25 20:11:37 +0800

    write data buffer in chunks

--- gc2_firmware/data_collection/main.cpp
+++ gc2_firmware/data_collection/main.cpp
@@ -24,10 +24,12 @@ TCPClient gc_client;
 #define DEBUG_LOG(x) serial_log(x)
 
 #define HANDSHAKE_BUFFER_LENGTH 64
-#define BUFFER_LENGTH 5766
+#define BUFFER_HEADER_LENGTH 8
+#define BUFFER_LENGTH 28808
+#define CHUNK_SIZE 1024
 char handshake_buffer[HANDSHAKE_BUFFER_LENGTH];
 char send_buffer[BUFFER_LENGTH];
-uint32_t current_offset = 0;
+uint32_t current_offset = BUFFER_HEADER_LENGTH;
 uint16_t num_datapoints = 0;
 
 #define GC_MODE_REALTIME 1
@@ -38,6 +40,7 @@ int gc_server_connect(String command);
 int gc_server_disconnect(String command);
 int gc_server_send_data(String command);
 int set_mode(String command);
+int device_util(String command);
 
 
 void reset_batch_buffer();
@@ -48,11 +51,26 @@ void serial_log(String message) {
   }
 }
 
+int device_util(String command) {
+  if(command=="test_serial") {
+    DEBUG_LOG("serial test");
+    return 0;
+  }
+
+  if(command=="setup_serial") {
+    Serial.begin(9600);
+    return 0;
+  }
+}
+
 void setup() {
   pinMode(A0, INPUT);
 
   if (serial_debug) {
     Serial.begin(9600);
+    // DEBUG_LOG("hit Enter to continue");
+    // while(!Serial.available()) Particle.process();
+    DEBUG_LOG("serial setup OK");
   }
 
 
@@ -69,7 +87,7 @@ void setup() {
 
   Particle.function("gc_conn", gc_server_connect);
   Particle.function("gc_disc", gc_server_disconnect);
-  Particle.function("gc_send_1", gc_server_send_data);
+  Particle.function("device_util", device_util);
   Particle.function("set_mode", set_mode);
 
 
@@ -78,6 +96,8 @@ void setup() {
   lipo.quickStart();
 
   Particle.publish("startup");
+
+  DEBUG_LOG("started up");
 
 }
 
@@ -237,7 +257,7 @@ int gc_server_send_data_point() {
 }
 
 void reset_batch_buffer() {
-  current_offset = 0;
+  current_offset = BUFFER_HEADER_LENGTH;
   num_datapoints = 0;
 }
 
@@ -262,29 +282,31 @@ int gc_server_upload_batch() {
   // indicate that we're uploading a batch
   gc_server_initial_handshake(GC_MODE_BATCH);
 
-  int bytes_written;
+
 
   // write data header
   uint32_t temp_offset = 0;
   // write battery charge
   uint16_t percent_charged = lipo.getSOC() * 100.0;
-  write_int16_to_buffer(handshake_buffer, percent_charged, &temp_offset);
+  write_int16_to_buffer(send_buffer, percent_charged, &temp_offset);
   // indicate how many datapoints we're sending
-  write_int16_to_buffer(handshake_buffer, num_datapoints, &temp_offset);
+  write_int16_to_buffer(send_buffer, num_datapoints, &temp_offset);
   // indicate total buffer size
-  write_int_to_buffer(handshake_buffer, current_offset, &temp_offset);
-  bytes_written = gc_client.write((const uint8_t *) handshake_buffer, temp_offset);
-  if(bytes_written != temp_offset) {
-    DEBUG_LOG("didn't write expected number of bytes");
-  }
-  gc_client.flush();
+  write_int_to_buffer(send_buffer, current_offset, &temp_offset);
 
-
-
-  // send full data buffer
-  bytes_written = gc_client.write((const uint8_t *) send_buffer, current_offset);
-  if(bytes_written != current_offset) {
-    DEBUG_LOG("didn't write expected number of bytes");
+  uint32_t written_so_far = 0;
+  uint32_t bytes_written;
+  uint32_t need_to_write;
+  uint32_t message_size;
+  while(written_so_far < current_offset) {
+    need_to_write = current_offset - written_so_far;
+    message_size = min(need_to_write, CHUNK_SIZE);
+    DEBUG_LOG("need_to_write: " + String(need_to_write) + " message_size: " + String(message_size));
+    bytes_written = gc_client.write((const uint8_t *) send_buffer + written_so_far, min(need_to_write, CHUNK_SIZE));
+    written_so_far += bytes_written;
+    if(bytes_written != message_size) {
+      DEBUG_LOG("wrote less than expected");
+    }
   }
   gc_client.flush();
 
