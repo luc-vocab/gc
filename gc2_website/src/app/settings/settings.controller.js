e9b96df0a1543ed40fd3d48cd74e7f9b4014c91d
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-11-21 14:31:06 +0800

    remove all jshint warnings

--- gc2_website/src/app/settings/settings.controller.js
+++ gc2_website/src/app/settings/settings.controller.js
@@ -1,3 +1,5 @@
+/* globals spark: false */
+
 (function() {
   'use strict';
 
@@ -9,36 +11,36 @@
   function SettingsController($timeout, $log, $q, $scope, $rootScope, $firebaseObject, currentAuth, firebase_auth, device_manager) {
     var vm = this;
     
-    $scope.show_status = {};
+    vm.show_status = {};
     
    
-    this.init = function() {
+    vm.init = function() {
         // subscribe to user object
         var user_ref = firebase_auth.get_user_ref(currentAuth.uid);
-        $scope.user_obj = $firebaseObject(user_ref);
+        vm.user_obj = $firebaseObject(user_ref);
         
-        $scope.user_obj.$loaded(function(data){
+        vm.user_obj.$loaded(function(){
             vm.verify_token();
         });
         
     }
     
-    this.list_devices = function() {
+    vm.list_devices = function() {
         $log.info("list_devices");
         
-        $scope.show_status.listing_devices = true;
+        vm.show_status.listing_devices = true;
         
-        $scope.show_status.listing_device_class = "alert-info";
+        vm.show_status.listing_device_class = "alert-info";
         
-        $scope.show_status.listing_devices_ok = false;
-        $scope.show_status.listing_devices_error = false;
+        vm.show_status.listing_devices_ok = false;
+        vm.show_status.listing_devices_error = false;
         
         var devicesPr = spark.listDevices();
         devicesPr.then(
           function(devices){
             $log.info('Devices: ', devices);
-            $scope.show_status.listing_devices_ok = true;
-            $scope.show_status.listing_device_class = "alert-success";
+            vm.show_status.listing_devices_ok = true;
+            vm.show_status.listing_device_class = "alert-success";
             
             vm.devices = devices;
             
@@ -46,31 +48,31 @@
           },
           function(err) {
             $log.error('List devices call failed: ', err);
-            $scope.show_status.listing_devices_error = true;
-            $scope.show_status.listing_devices_error_text = err.toString();
-            $scope.show_status.listing_device_class = "alert-danger";
+            vm.show_status.listing_devices_error = true;
+            vm.show_status.listing_devices_error_text = err.toString();
+            vm.show_status.listing_device_class = "alert-danger";
             $scope.$apply();
           }
         );                        
     };
     
-    this.verify_token = function() {
+    vm.verify_token = function() {
         $log.info("verify_token");
         
-        var login_promise = device_manager.spark_login($scope.user_obj.particle_access_token);
+        var login_promise = device_manager.spark_login(vm.user_obj.particle_access_token);
         
-        login_promise.then(function(token) {
+        login_promise.then(function() {
             $log.info("spark login successful");
             vm.list_devices();
            
         },
         function(error) {
-            $log.error("spark login error");
+            $log.error("spark login error: ", error);
         });
     }
     
     
-    this.init();
+    vm.init();
     
   }
 })();
