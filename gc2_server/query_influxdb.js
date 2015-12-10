ee40add09909524b0254b4fd4bfa6665b6ad15f5
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-12-10 23:44:52 +0800

    add percentile queries

--- gc2_server/query_influxdb.js
+++ gc2_server/query_influxdb.js
@@ -16,7 +16,8 @@ influent
 })
 .then(function(client) {
 	
-	var query_latest = "select * from emg where time > now() - 30m";
+	var query_latest = "select * from emg where time > now() - 30m and time < now() - 29m";
+	var query_percentile = "select percentile(emg_value,85) from emg where time > now() - 30m";
 	var query = "select count (emg_value) from emg";
 	
 	// perform query here
@@ -31,3 +32,20 @@ influent
             });
 	
 });
+
+
+/*
+query ideas
+// get the value at the 85th percentile
+select percentile(emg_value,85) from emg where time > now() - 30m
+
+// sum all datapoints above that value
+select sum(emg_value) from emg where time > now() - 30m and emg_value > 121
+
+// count all datapoints above that value
+select count(emg_value) from emg where time > now() - 30m and emg_value > 121
+
+// then subtract the 2
+sum - count*threshold
+
+*/
