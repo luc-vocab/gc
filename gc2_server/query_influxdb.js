fa7e9532a167f817776805ebc182d845d2bfe17e
Luc Wastiaux <luc.wastiaux@airpost.net>
2015-12-11 22:58:46 +0800

    determine total score

--- gc2_server/query_influxdb.js
+++ gc2_server/query_influxdb.js
@@ -15,17 +15,35 @@ influent
     ]
 })
 .then(function(client) {
-	
-	var query_latest = "select * from emg where time > now() - 30m and time < now() - 29m";
-	var query_percentile = "select percentile(emg_value,85) from emg where time > now() - 30m";
-	var query = "select count (emg_value) from emg";
+
+	var query_percentile = "select percentile(emg_value,85) from emg where time > now() - 30m";	
 	
 	// perform query here
 	client
-            .query(query_latest)
+            .query(query_percentile)
             .then(function(result) {
-                // console.log(result.results[0]);
-                console.log(util.inspect(result, {showHidden: false, depth: null}));
+                // console.log(util.inspect(result, {showHidden: false, depth: null}));
+                var threshold_value = result.results[0].series[0].values[0][1];
+                console.log("threshold_value: ", threshold_value);
+                
+                var query_sum = "select sum(emg_value) from emg where time > now() - 30m and emg_value > " + threshold_value;
+                client.query(query_sum).then(function(result) {
+                    console.log(util.inspect(result, {showHidden: false, depth:null}));                                        
+                    var sum = result.results[0].series[0].values[0][1];
+                    console.log("sum: ", sum);
+                    
+                    var query_count = "select count(emg_value) from emg where time > now() - 30m and emg_value > " + threshold_value;
+                    client.query(query_count).then(function(result) {
+                        console.log(util.inspect(result, {showHidden: false, depth:null}));
+                        var count = result.results[0].series[0].values[0][1];
+                        console.log("count: ", count);
+                        
+                        var total_score = sum - count * threshold_value;
+                        console.log("total_score: ", total_score);
+                        
+                    });
+                });
+                
             }, function(error) {
             	console.log("error: ", error);
             	
