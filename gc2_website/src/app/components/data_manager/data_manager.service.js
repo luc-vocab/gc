7a25d15b8a60adf630664cb34c53caf445d255ff
lucw <luc.wastiaux@xsmail.com>
2015-12-20 05:31:37 +0000

    working EMG chart from influx data

--- gc2_website/src/app/components/data_manager/data_manager.service.js
+++ gc2_website/src/app/components/data_manager/data_manager.service.js
@@ -1,5 +1,6 @@
 /* globals Firebase: false */
 /* globals angular */
+/* globals influent */
 
 (function() {
     'use strict';
@@ -9,14 +10,52 @@
         .service('data_manager', data_manager);
 
     /** @ngInject */
-    function data_manager(firebase_root) {
+    function data_manager(firebase_root, influx_config, $log, $q) {
         var root_ref = new Firebase(firebase_root);
         var data_ref = root_ref.child('data');
+        
+        var self = this;
         
         this.latest_data_ref = function(uid) {
             return data_ref.child(uid).child('latest');
         };
         
+     
+        this.get_influx_client = function() {
+            return influent.createHttpClient({
+                username: influx_config.username,
+                password: influx_config.password,
+                database: influx_config.db,
+                server: [
+                    {
+                        protocol: "http",
+                        host: influx_config.host,
+                        port: influx_config.port
+                    }
+                ]
+            });
+        };
+        
+        this.get_latest_emg_data = function(username, start_time, end_time) {
+            var defer = $q.defer();
+            
+
+            var username_clause = "username='" + username + "'";
+            var time_clause = "time > '" + start_time.toISOString() + "' and time < '" + end_time.toISOString() + "'";
+            var query = "select MAX(emg_value) from emg where " + username_clause + 
+                        " and " + time_clause + " GROUP BY time(1s)";
+            $log.info("latest_emg_data query: ", query);
+
+            self.get_influx_client().then(function(client) {
+                client.query(query).then(function(result) {
+                   $log.info("result: ", result); 
+                   var data = result.results[0].series[0].values;
+                   defer.resolve(data);
+                });
+            });                              
+          
+            return defer.promise;
+        };
         
         return this;
 
--- gc2_website/src/app/latest/latest.controller.js
+++ gc2_website/src/app/latest/latest.controller.js
@@ -1,5 +1,6 @@
 
 /* globals angular */
+/* globals Highcharts */
 
 (function() {
   'use strict';
@@ -17,8 +18,85 @@
     vm.init = function() {
         // bind to the latest data object for this user
         vm.uid = currentAuth.uid;
+        
+        var user_ref = firebase_auth.get_user_ref(vm.uid);
+        vm.user_obj = $firebaseObject(user_ref);
+        
         var latest_data_ref = data_manager.latest_data_ref(vm.uid);
         vm.latest_data_obj = $firebaseObject(latest_data_ref);
+        
+        vm.user_obj.$loaded().then(function(){
+          vm.latest_data_obj.$loaded().then(function(){
+            vm.load_data();
+          })
+        })
+    };
+    
+    vm.load_data = function() {
+      $log.info("LatestController.load_data");
+      var start_time = new Date(vm.latest_data_obj.end_timestamp - 300*1000);
+      var end_time = new Date(vm.latest_data_obj.end_timestamp);
+      data_manager.get_latest_emg_data(vm.user_obj.user_name, 
+                                       start_time,
+                                       end_time).
+      then(function(data) {
+        // plot data on chart
+        vm.chart_emg_data(data);  
+      })
+    };
+    
+    vm.chart_emg_data = function(data) {
+        $('#container-emg-chart').highcharts({
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Latest EMG data'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'EMG Value'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                name: 'EMG',
+                data: data
+            }]
+        });      
     };
 
 
