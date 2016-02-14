5b56eaf445711420ef723dcd9f11cee081da6ece
lucw <luc.wastiaux@xsmail.com>
2016-02-14 23:27:00 +0000

    remove blog_site on clean

--- gc2_website/gulp/build.js
+++ gc2_website/gulp/build.js
@@ -94,7 +94,9 @@ gulp.task('other', function () {
 });
 
 gulp.task('clean', function () {
-  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+  return $.del([path.join(conf.paths.dist, '/'), 
+                path.join(conf.paths.tmp, '/'),
+                path.join(conf.paths.blog_site, '/')]);
 });
 
 gulp.task('build', ['html', 'fonts', 'other', 'build_and_copy_blog']);
