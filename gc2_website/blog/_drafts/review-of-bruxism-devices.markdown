89e544c25590c06f4f2f81529ae55d79332cb1df
lucw <luc.wastiaux@xsmail.com>
2016-02-20 04:12:06 +0000

    in dev mode, generate blog with drafts

---
+++ gc2_website/blog/_drafts/review-of-bruxism-devices.markdown
@@ -0,0 +1,23 @@
+---
+layout: post
+title: "Review of Bruxism Devices on the market"
+section: blog
+---
+
+There are a number of devices that help monitor or attempt to prevent bruxism on the market. 
+
+Grindcare
+---------
+
+I purchased the grindcare blablabla
+
+Sleepguard
+----------
+
+
+StatDDS
+-------
+
+Bruxoff
+-------
+
--- gc2_website/gulp/blog.js
+++ gc2_website/gulp/blog.js
@@ -9,9 +9,28 @@ gulp.task('copy_blog_files', function() {
   .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve')));
 });
 
-gulp.task('build_blog', shell.task([
+gulp.task('build_blog_dev', shell.task([
+  'jekyll build --drafts'
+], {cwd: conf.paths.blog_src}));
+
+
+gulp.task('build_blog_prod', shell.task([
   'jekyll build'
 ], {cwd: conf.paths.blog_src}));
+
+gulp.task('build_blog', function() {
+  var blog_env = process.env.ENV;
+  if(!blog_env) {
+    throw "ENV not set [ENV=dev]";
+  }  
+  
+  if(blog_env == "dev") {
+    return gulp.start('build_blog_dev');
+  } else {
+    return gulp.start('build_blog_prod');
+  }
+  
+})
 
 gulp.task('build_and_copy_blog', ['build_blog'], function() {
   return gulp.start('copy_blog_files');
