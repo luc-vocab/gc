f82eead00ed2cddcf5aa6d533b0462dd70a99c34
lucwastiaux <luc.wastiaux@airpost.net>
2016-09-25 12:28:58 +0800

    working streaming of battery state

--- gc2_firmware/sleeptrack_diagnostics/diagnostics.cpp
+++ gc2_firmware/sleeptrack_diagnostics/diagnostics.cpp
@@ -5,17 +5,20 @@
 #define BUTTON1_PIN D2
 #define BUTTON2_PIN D3
 
-bool s_battery_setup = false;
-bool s_stream_battery = false;
-
 bool s_report_button1_state = false;
 bool s_report_button2_state = false;
+
+bool s_start_battery = false;
+bool s_report_battery_state = false;
 
 int start_batt(String input);
 int stop_batt(String input);
 int test_spkr(String input);
 void button1();
 void button2();
+void report_battery_state();
+
+Timer battery_timer(5000, report_battery_state);
 
 void setup() {
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
@@ -44,11 +47,13 @@ void button2()
 
 int start_batt(String input)
 {
+  s_start_battery = true;
   return 0;
 }
 
 int stop_batt(String input)
 {
+  battery_timer.stop();
   return 0;
 }
 
@@ -60,6 +65,21 @@ int test_spkr(String input)
 
   return 0;
 };
+
+void setup_battery()
+{
+  lipo.begin(); // Initialize the MAX17043 LiPo fuel gauge
+	lipo.quickStart();
+
+  Particle.publish("battery", "battery setup complete");
+
+  battery_timer.start();
+}
+
+void report_battery_state()
+{
+  s_report_battery_state = true;
+}
 
 void loop() {
   if (s_report_button1_state)
@@ -74,6 +94,21 @@ void loop() {
     bool button2_state = digitalRead(BUTTON2_PIN);
     Particle.publish("button2", String(button2_state));
     s_report_button2_state = false;
+  }
+
+  if(s_start_battery)
+  {
+    setup_battery();
+    s_start_battery = false;
+  }
+
+  if (s_report_battery_state)
+  {
+    double voltage = lipo.getVoltage();
+  	double soc = lipo.getSOC();
+
+    Particle.publish("battery", "volt: " + String(voltage) + " pct: " + String(soc));
+    s_report_battery_state = false;
   }
 
   delay(250);
