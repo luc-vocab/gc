2c8207212a4b7f1437a1ce26ac2843693f6677a8
Luc Wastiaux <luc.wastiaux@airpost.net>
2017-02-10 23:36:59 +0800

    adapt to firebase 3 sdk

--- gc2_server/gc_config.js
+++ gc2_server/gc_config.js
@@ -1,4 +1,4 @@
-var Firebase = require('firebase');
+var admin = require("firebase-admin");
 var q = require('promised-io/promise');
 var influent = require('influent');
 var winston = require('winston');
@@ -10,18 +10,40 @@ var GcConfig = function(server_type) {
 
     // retrieve influxdb and papertrail configs
 
+    // configure firebase
+    var firebase_config_file = process.env.FB_CONFIG_FILE;
+    if(!firebase_config_file)
+    {
+        throw "FB_CONFIG_FILE not set";
+    }
+    
     // retrieve config from firebase
     var firebase_root = process.env.FB_ROOT;
     if (!firebase_root) {
         throw "FB_ROOT not set [FB_ROOT=gcserver-dev.firebaseio.com]";
-    }
+    }    
 
-    var firebase_root = 'https://' + firebase_root + '/';
-    var firebase_root_ref = new Firebase(firebase_root);
-    var config_ref = firebase_root_ref.child('config');
+    // Fetch the service account key JSON file contents
+    var serviceAccount = require(firebase_config_file);
+    
+
+    // Initialize the app with a null auth variable, limiting the server's access
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://"+ firebase_root +".firebaseio.com"
+    });
+    
+    // for debugging of authentication
+    // admin.database.enableLogging(true);
+    
+    // retrieve configuration
+    var db = admin.database();
+    var config_ref = db.ref("config");
+     
     config_ref.once('value', function(snapshot) {
-        var config = snapshot.val();
 
+        var config = snapshot.val();
+        
 
         // create logger
         var papertrailTransport = new winston.transports.Papertrail({
@@ -74,7 +96,7 @@ var GcConfig = function(server_type) {
                     }
 
                     // retrieve server config
-                    var server_ref = firebase_root_ref.child('servers').child(server_key);
+                    var server_ref = db.ref('/servers').child(server_key);
 
                     server_ref.once('value', function(snapshot) {
                         var server_config = snapshot.val();
@@ -105,9 +127,8 @@ var GcConfig = function(server_type) {
             });
 
     }, function(err) {
-        console.err("couldn't retrieve config: ", err);
+        console.log("couldn't retrieve config: ", err);
     });
-
 
 
     return defer.promise;
